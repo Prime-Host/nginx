7a28f4eb3e623f9488f6558de88a22b9028beecc
Nurian <gregorius.stahlbart@gmail.com>
2017-03-22 17:56:26 +0100

    add ssh user and password

--- nginx-start.sh
+++ nginx-start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Create custom ssh_user with sudo privileges
+useradd -m -d /home/$PRIMEHOST_USER -G root -s /bin/bash $PRIMEHOST_USER \
+	&& usermod -a -G $PRIMEHOST_USER $PRIMEHOST_USER \
+	&& usermod -a -G sudo $PRIMEHOST_USER
+
+# Set passwords for the ssh_user and root
+echo "$PRIMEHOST_USER:$PRIMEHOST_PASSWORD" | chpasswd
+echo "root:$PRIMEHOST_PASSWORD" | chpasswd
+
 # start all the services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
