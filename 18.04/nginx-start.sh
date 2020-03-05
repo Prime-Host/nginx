6f9913d6574a0b0b51b9d2c8d77268714f0b7f43
root <root@vmd37390.contaboserver.net>
2020-03-05 11:34:02 +0100

    add nginx with ubuntu 18.04

---
+++ 18.04/nginx-start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create index.php only if there is none
+if [ ! -f /var/www/html/index.php ]; then
+  echo "<?PHP
+  phpinfo();" >> /var/www/html/index.php
+fi
+
+# make all user changes only on first creation or if the user changed
+if [ ! -f /home/$PRIMEHOST_USER ]; then
+  # Create custom ssh_user with sudo privileges
+  useradd -m -d /home/$PRIMEHOST_USER -G root -s /bin/zsh $PRIMEHOST_USER \
+  && usermod -a -G $PRIMEHOST_USER $PRIMEHOST_USER \
+  && usermod -a -G sudo $PRIMEHOST_USER
+
+  # Set passwords for the custom user and root
+  echo "$PRIMEHOST_USER:$PRIMEHOST_PASSWORD" | chpasswd
+  echo "root:$PRIMEHOST_PASSWORD" | chpasswd
+
+  # Custom user for nginx 
+  sed -i s/www-data/$PRIMEHOST_USER/g /etc/nginx/nginx.conf
+  chown -R ${PRIMEHOST_USER}:${PRIMEHOST_USER} /var/www/html
+fi
+
+# start all the services
+/usr/bin/supervisord
